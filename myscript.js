b6bca3454a504d51421bfffb69bca71b765aca05
sibekov <vusimuzij.sibeko@gmail.com>
2024-12-07 14:38:35 +0000

    Add a while loop if ans incorrect

--- myscript.js
+++ myscript.js
@@ -4,15 +4,15 @@ var age=false
 var height=false
 var pet="tiger"
 
-while(spyname=false){
+while(true){
 spyname=prompt("enter your first name: ")
 spylastname=prompt("enter your last name: ")
 age=prompt("enter your age: ")
 height=prompt("enter your height: ")
 pet=prompt("enter your pet's name: ")
-if(spyname[0]==spylastname[0] && (age<30 && age >20) && height<175 && pet[-1]=="y"){
-    {console.log("Welcome to the game spy!!!!")}
+
+if((spyname[0]===spylastname[0]) && (age<30 && age >20) && (height>=175) && (pet[pet.length-1]==="y"))
+    {console.log("Welcome to the game spy!!!!")
     break
-}
-else{console.log("you are taking chances...")}
-}
+};}
+
